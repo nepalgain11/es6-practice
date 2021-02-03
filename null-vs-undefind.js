f2d7b2522261a73495b8ff4907543de0c752e085
Nepal Gain <nepalgain11@gmail.com>
2021-02-03 20:00:40 +0600

    jjj

---
+++ null-vs-undefind.js
@@ -0,0 +1,16 @@
+//let name;
+
+// function add (nub1, nub2){
+//     console.log(nub1 + nub2);
+//     return;
+// }
+
+// function add (nub1, nub2){
+//     console.log(nub1 , nub2); 
+// }
+
+
+const person  = {name : "nepal", age :23, phone : 01722510113}
+
+// const person = add(12);
+console.log(person.gf);
