06a74fc6d05608a4382d168ae8e1b03f5c5fd01f
Nepal Gain <nepalgain11@gmail.com>
2021-02-04 20:00:01 +0600

    arrow function map/find/filter

--- null-vs-undefind.js
+++ null-vs-undefind.js
@@ -1,16 +1,71 @@
-//let name;
-
-// function add (nub1, nub2){
-//     console.log(nub1 + nub2);
-//     return;
+//const numbers = [3,4,5,6,7];
+// const output = [];
+// for (let i = 0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result =element*element;
+//     output.push(result);
 // }
 
-// function add (nub1, nub2){
-//     console.log(nub1 , nub2); 
+// const numbers = [3,4,5,6,7];
+// const result = numbers.map(function(element, index, array){
+//   return element * element;
+// })
+
+
+
+// console.log(result);
+
+// function add(addd , adadd){
+//     var  res = addd + adadd;
+//     return res;
+// }
+// console.log(add(12,12));
+// const squire =element =>element*element;
+// console.log(squire(12));
+
+
+
+
+// const numbers = [3,4,5,6,7];
+// const output = [];
+// for (let i = 0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result =element*element;
+//     output.push(result);
 // }
 
+// const numbers = [3,4,5,6,7];
+// const result = numbers.map(function(element, index, array){
+//   return element * element;
+// })
+// console.log(result);
+// -------------------------------------
+// const numbers = [3,4,5,6,7];
+// const result = numbers.map(add=>add*add);
 
-const person  = {name : "nepal", age :23, phone : 01722510113}
+// console.log(result);
 
-// const person = add(12);
-console.log(person.gf);
+// const numbers = [9,3,4,5,6,7];
+// const result = numbers.filter(x=> x>5);
+// console.log(result);
+
+// const numbers = [9,3,4,5,6,7];
+// const result = numbers.filter(x => x > 5);
+// console.log(result);
+
+
+const studenst = [
+    {id:21, name:"omor sunny"},
+    {id:31, name:"manna sunny"},
+    {id:41, name:"deepjol sunny"},
+    {id:51, name:"hero alam"}
+];
+// for (let i = 0; i<studenst.length; i++){
+//     const getName = studenst.object[name];
+//     console.log(getName);
+// }
+
+const name = studenst.map(sss =>sss.name);
+console.log(name);
+
+
